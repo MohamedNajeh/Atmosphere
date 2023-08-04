1bab59648917c056c0c64c8cbd73431e43622b98
Mohamed Najeh <mohamed.najeh.ahmed@gmail.com>
2023-08-04 15:25:47 +0300

    added regidter stmt

--- AtmosphereLec/MoviewList/MovieCell.swift
+++ AtmosphereLec/MoviewList/MovieCell.swift
@@ -13,6 +13,8 @@ class MovieCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        print("registerBranch")
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
